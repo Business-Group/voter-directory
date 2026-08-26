399ca17c07955e2749e214df33bd58665fca6cc1
Huzaifa Saqib <huzaifasaqib6972@gmail.com>
2026-08-27 01:18:34 +0500

    Change base URL in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'voter-directory' with your exact GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/voter-directory/', 
+  base: '/', 
 })
